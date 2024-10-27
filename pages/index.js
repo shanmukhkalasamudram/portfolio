ef563335d6e68e23b89b0bcbb450fa52b46bed4b
SHANMUKH SAI <50857103+shanmukhkalasamudram@users.noreply.github.com>
2024-10-27 12:40:14 -0400

    Updated JSON Content file

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,7 @@ export default function Main({ footerData, photographyData }) {
   const fetchApiEndPoint = async () => {
     try {
       const { data } = await axios.get(
-        "https://res.cloudinary.com/dydnnxrft/raw/upload/v1722921220/aug6_template.json"
+        "https://res.cloudinary.com/dydnnxrft/raw/upload/v1730047072/oct_23_template.json"
       );
       return data;
     } catch (err) {
